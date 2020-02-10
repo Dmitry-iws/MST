5eccce03c2dad51111c629dee6694ecfe8034a32
Dmitry-IWS <dmitry1991.my@mail.ru>
2020-02-10 15:56:05 +0300

    initial

---
+++ src/View/MainPage/MainPage.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './MainPage.module.sass'
+
+const MainPage = () => {
+  return <div className={styles.MainPage}>MainPage</div>
+}
+
+export default MainPage
